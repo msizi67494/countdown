d0eea3a3b6655c38a00c76ff12014162600b854d
msizi67494 <msizi.ndlela5@gmail.com>
2018-11-07 11:44:01 +0200

    adding moment.js library to simply the countdown calculation

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
         <Countdown/>
       </div>
       
--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React,{ Component } from 'react'
 
-const Countdown = (props) => {
-    return <div>
-              <h1>New year is coming soon</h1>
+class Countdown extends Component {
+
+    constructor(){
+
+    }
+    
+    render(){
+        return (
+            <div>
+                <section className="hero is-info  is-bold is-fullheight">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title has-text-centered">
+                                New Year is Coming Soon!
+                            </h1>
+                            <div className="section">
+                                <nav className="level">
+                                    <div className="level-item has-text-centered">
+                                        <div>
+                                            <p className="heading">Days</p>
+                                            <p className="title">3,456</p>
+                                        </div>
+                                    </div>
+                                        <div className="level-item has-text-centered">
+                                            <div>
+                                            <p className="heading">Hours</p>
+                                            <p className="title">123</p>
+                                            </div>
+                                        </div>
+                                        <div className="level-item has-text-centered">
+                                            <div>
+                                            <p className="heading">Minutes</p>
+                                            <p className="title">456</p>
+                                            </div>
+                                        </div>
+                                        <div className="level-item has-text-centered">
+                                            <div>
+                                            <p className="heading">Seconds</p>
+                                            <p className="title">789</p>
+                                            </div>
+                                        </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
+        );
+    }
 }
+    
 export default Countdown;
